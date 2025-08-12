151e4e1693cb90ed396e969fdea96a090e10a186
anderkan <danp2002foster@gmail.com>
2025-08-12 12:24:10 +1200

    Messing around

--- music_starter.js
+++ music_starter.js
@@ -1,43 +1,48 @@
+let size = 0;
+let bassM = 0;
+
+
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
-  textFont('Verdana'); // please use CSS safe fonts
-  rectMode(CENTER)
-  textSize(24);
-  
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
- 
-// changes 
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
- 
-   // display "words"
-   textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
+
+   size = map(vocal,1,100,100,250);
+   bassM = map(vocal,0,100,0,20);
+
+   drawSphere(size,bassM);
+   forLoop(size,bassM);
+}
+
+
+function drawSphere(size,bassM){
+   
+   background(20);
+
+   push();
+   pointLight(255,255,255,200,-200,200);
+   specularMaterial(0,255,0);
+   shininess(50);
+   fill(0,0,0);
+
+   rotateWithFrameCount();
+   stroke(0,255,255);
+   strokeWeight(5);
+   noFill();
+   sphere(size,10,10);
+   pop();
+
+}
+
+function forLoop(size,bassM){
+   translate(0,400);
+   for(let i = 0; i<bassM; i ++){
+      rect(300,50*-i,100,50);
+      fill(255,255-(18*i),0);
+
+   }
+}
+
+function rotateWithFrameCount(){
+   rotateY(frameCount);
+   rotateX(frameCount);
 }
